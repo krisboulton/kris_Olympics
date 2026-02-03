83471a43f628c648dd35f8bb59307b0b1c41f0a8
krisboulton <35156381+krisboulton@users.noreply.github.com>
2026-02-02 19:45:00 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,11 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import CanadianOlympics from "./App.jsx"; // <-- this should point to the file that contains your big component
+
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <CanadianOlympics />
+  </React.StrictMode>
+);
+
 
